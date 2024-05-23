cc22b0c595a89970fdf3e6a38519abe7a22f64e0
marioliveira-mbi <mariana.oliveira@mb.com.br>
2024-05-23 14:52:36 -0300

    Criando matriculo

--- escola/admin.py
+++ escola/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 
 from django.contrib import admin
-from escola.models import Aluno, Curso
+from escola.models import Aluno, Curso, Matricula
 
 class Alunos(admin.ModelAdmin):
-        list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento')
-        list_display_links = ('id', 'nome')
-        search_fields = ('nome',)
-        list_per_page = 20
+    list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+    list_per_page = 20
 
 admin.site.register(Aluno, Alunos)
 
@@ -17,3 +17,9 @@ class Cursos(admin.ModelAdmin):
     search_fields = ('codigo_curso',)
 
 admin.site.register(Curso, Cursos)
+
+class Matriculas(admin.ModelAdmin):
+        list_display = ('id', 'aluno', 'curso', 'periodo')
+        list_display_links = ('id',)
+        
+admin.site.register(Matricula, Matriculas)
